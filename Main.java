0cab716835051902e882b729fd9a848167b9b4cf
kikuchiyo <2674866249@qq.com>
2026-03-15 23:24:15 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,20 @@
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+
 public class Main {
 
-    public static void main(String[] args) throws UnknownHostException {
-        InetAddress address = InetAddress.getByName("LAPTOP-DNMC2DQ7");
-        System.out.println(address);
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+        if (s1.contains(s2)){
+            System.out.println(s1.indexOf(s2));
+        } else {
+            System.out.println(-1);
+        }
     }
 }
-
 
 
 
